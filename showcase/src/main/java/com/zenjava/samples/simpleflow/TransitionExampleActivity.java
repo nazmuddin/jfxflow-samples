55e70630010e44e54cd9970a635cfd8bdd555efd
zonski <zonski@609bac98-73c8-619a-997c-37ed6927f7a9>
2011-11-25 22:52:40 +0000

    Added transitions

---
+++ showcase/src/main/java/com/zenjava/samples/simpleflow/TransitionExampleActivity.java
@@ -0,0 +1,59 @@
+package com.zenjava.samples.simpleflow;
+
+import com.zenjava.jfxflow.actvity.AbstractActivity;
+import com.zenjava.jfxflow.transition.*;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.util.Duration;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.annotation.PostConstruct;
+import java.util.ResourceBundle;
+
+public class TransitionExampleActivity extends AbstractActivity
+        implements HasEntryTransition, HasExitTransition
+{
+    enum Type { fade, fly }
+
+    @FXML private Label messageLabel;
+    @Autowired private ResourceBundle resources;
+    private Type type;
+
+    public void setType(Type type)
+    {
+        this.type = type;
+    }
+
+    @PostConstruct
+    private void init()
+    {
+        messageLabel.setText(resources.getString(String.format("transitionExample.message.%s", type.name())));
+    }
+
+    public ViewTransition getEntryTransition()
+    {
+        switch (type)
+        {
+            case fade:
+                return new FadeInTransition(getNode(), Duration.millis(500));
+            case fly:
+                return FlyTransition.createFlyIn(getNode(), Duration.millis(500), VerticalPosition.top);
+            default:
+                throw new IllegalArgumentException(String.format("Unsupported type: %s", type));
+        }
+    }
+
+    public ViewTransition getExitTransition()
+    {
+        switch (type)
+        {
+            case fade:
+                return new FadeOutTransition(getNode(), Duration.millis(500));
+            case fly:
+                return FlyTransition.createFlyOut(getNode(), Duration.millis(500),
+                        HorizontalPosition.right, VerticalPosition.bottom);
+            default:
+                throw new IllegalArgumentException(String.format("Unsupported type: %s", type));
+        }
+    }
+}
