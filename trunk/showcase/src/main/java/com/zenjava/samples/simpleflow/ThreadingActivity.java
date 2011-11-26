0f0172836264a7bdce3e92c4a5ecc9a86389849a
zonski <zonski@609bac98-73c8-619a-997c-37ed6927f7a9>
2011-11-26 00:01:14 +0000

    Added transitions

--- trunk/showcase/src/main/java/com/zenjava/samples/simpleflow/ThreadingActivity.java
+++ trunk/showcase/src/main/java/com/zenjava/samples/simpleflow/ThreadingActivity.java
@@ -1,10 +1,89 @@
 package com.zenjava.samples.simpleflow;
 
 import com.zenjava.jfxflow.actvity.AbstractActivity;
-import com.zenjava.jfxflow.navigation.NavigationManager;
+import com.zenjava.jfxflow.worker.BackgroundTask;
+import com.zenjava.jfxflow.worker.ErrorHandler;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressIndicator;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ResourceBundle;
 
 public class ThreadingActivity extends AbstractActivity
 {
-    @Autowired private NavigationManager navigationManager;
+    @FXML Label statusLabel;
+    @FXML Label customStatusLabel;
+    @FXML Button customTaskButton;
+    @FXML ProgressIndicator progressIndicator;
+
+    @Autowired ResourceBundle resources;
+    @Autowired ErrorHandler errorHandler;
+
+    protected void activated()
+    {
+        statusLabel.setText(resources.getString("threading.block.custom.normal"));
+        customStatusLabel.setText(resources.getString("threading.block.custom.normal"));
+    }
+
+    public void startBackgroundTask(ActionEvent event)
+    {
+        statusLabel.setText(resources.getString("threading.block.started"));
+        executeTask(new BackgroundTask<Void>()
+        {
+            protected Void call() throws Exception
+            {
+                Thread.sleep(3000);
+                return null;
+            }
+
+            protected void onSuccess(Void value)
+            {
+                statusLabel.setText(resources.getString("threading.block.done"));
+            }
+        });
+    }
+
+    public void startCustomBackgroundTask(ActionEvent event)
+    {
+        BackgroundTask<Void> task = new BackgroundTask<Void>()
+        {
+            protected Void call() throws Exception
+            {
+                updateProgress(0, 100);
+                for (int i = 0; i < 100; i++)
+                {
+                    Thread.sleep(30);
+                    updateProgress(i, 100);
+                }
+                return null;
+            }
+
+            protected void onSuccess(Void value)
+            {
+                customTaskButton.setDisable(false);
+                progressIndicator.setVisible(false);
+                customStatusLabel.setText(resources.getString("threading.block.custom.done"));
+            }
+        };
+
+        customStatusLabel.setText(resources.getString("threading.block.custom.started"));
+        progressIndicator.progressProperty().bind(task.progressProperty());
+        customTaskButton.setDisable(true);
+        new Thread(task).start();
+        progressIndicator.setVisible(true);
+    }
+
+    public void startErrorBackgroundTask(ActionEvent event)
+    {
+        executeTask(new BackgroundTask<Void>(errorHandler)
+        {
+            protected Void call() throws Exception
+            {
+                throw new RuntimeException("An example exception. Note that this error page is stylable via CSS");
+            }
+        });
+    }
 }
