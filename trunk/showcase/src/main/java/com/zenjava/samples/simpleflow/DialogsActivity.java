8601c117cba34b99d6016ede7b27b6bdcba103cd
zonski <zonski@609bac98-73c8-619a-997c-37ed6927f7a9>
2011-11-26 00:35:02 +0000

    Updated

--- trunk/showcase/src/main/java/com/zenjava/samples/simpleflow/DialogsActivity.java
+++ trunk/showcase/src/main/java/com/zenjava/samples/simpleflow/DialogsActivity.java
@@ -1,10 +1,16 @@
 package com.zenjava.samples.simpleflow;
 
 import com.zenjava.jfxflow.actvity.AbstractActivity;
-import com.zenjava.jfxflow.navigation.NavigationManager;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.zenjava.jfxflow.dialog.Dialog;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
 
 public class DialogsActivity extends AbstractActivity
 {
-    @Autowired private NavigationManager navigationManager;
+    public void showSimpleDialog(ActionEvent event)
+    {
+        Dialog dialog = new Dialog("Test Dialog");
+        dialog.setContent(new Label("This is a test dialog"));
+        dialog.show(getNode());
+    }
 }
